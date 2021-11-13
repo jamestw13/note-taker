74dc622a1aa8b1ec775a6da701428d002b44b33e
TJ James <jamestw13@gmail.com>
2021-11-13 00:53:44 -0600

    added uuid functionality, POST is functional

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,13 +1,23 @@
 const router = require('express').Router();
 const db = require('../../db/db.json');
+const {v4: uuid} = require('uuid');
+const fs = require('fs');
+const path = require('path');
 
 router.get('/notes', (req, res) => {
 	res.json(db);
 });
 
 router.post('/notes', (req, res) => {
-	console.log(req.body);
-	db.push(req.body);
+	let note = req.body;
+	note.id = uuid();
+	db.push(note);
+	console.log(db);
+	fs.writeFileSync(path.join(__dirname, '../../db/db.json'), JSON.stringify(db));
+	res.json(db);
+});
+
+router.delete('/notes/:id', (req, res) => {
 	res.json(db);
 });
 
